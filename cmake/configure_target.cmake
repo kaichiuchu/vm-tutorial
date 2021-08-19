28604ac896862e7f5c404525985d6f34a6eda381
kaichiuchu <kaichiuchu@protonmail.com>
2021-08-19 18:19:19 -0400

    cmake: add initial structure
    This structure illustrates precisely the type of separation one should
    strive for; clear, unambiguous and readily identifiable components.
    
    This tutorial project officially only supports two compilers: g++ >=8.1,
    or clang++ >=5.0. If neither compilers are used, a warning will be
    emitted to the user but it is not an error. It is important to note that
    failure to use either one of these compilers may result in poor code
    generation (and therefore, poor performance) or a failed compile
    attempt. This tutorial project does not cover these scenarios.
    
    This tutorial project is written in C++17 with support for nonstandard
    extensions which is a partial violation of rule "P.2: Write in ISO
    Standard C++" of the C++ Core Guidelines. We violate it because we
    specifically only support two compilers, of which they are ubiquitous
    and accessible on almost every system. In addition, exceptions and RTTI
    are forbidden throughout the project and are forcibly disabled through
    compiler options if supported.
    
    Once a target has been created (e.g. through an `add_library()` call),
    it is imperative that one calls `vmtutorial_configure_target()` to set
    build flags on it relative to the specified build type, and to enforce
    the aforementioned.

---
+++ cmake/configure_target.cmake
@@ -0,0 +1,59 @@
+# vm-tutorial - Virtual machine tutorial targeting CHIP-8
+#
+# Written in 2021 by kaichiuchu <kaichiuchu@protonmail.com>
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+function(vmtutorial_configure_target TARGET_NAME)
+  # Throughout this tutorial, the C++17 standard will be required with the
+  # possibility of using extensions.
+  set_target_properties(${TARGET_NAME} PROPERTIES
+                        CXX_STANDARD 17
+                        CXX_STANDARD_REQUIRED YES
+                        CXX_EXTENSIONS ON)
+
+  # We do not support exceptions or RTTI throughout the tutorial, so disable
+  # them for the compilers we support.
+  #
+  # An exception to this rule is with clang if the frontend is clang-cl;
+  # neither exceptions nor RTTI can be disabled if this is the case as the
+  # following flags are not recognized, we'll just have to suck it up.
+  #
+  # g++ handles these flags just fine on every platform that I'm aware of.
+  #
+  # These flags are set regardless of the build type specified.
+  if ((CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
+       CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "GNU") OR
+       CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(MAIN_COMPILER_FLAGS
+        "-fno-exceptions;-fno-rtti;-fno-unwind-tables;-fno-asynchronous-unwind-tables")
+  endif()
+
+  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+      set(ADDITIONAL_COMPILER_FLAGS
+          "-Wall;-Werror;-Wextra;-Wno-c++98-compat;-Wno-c++98-compat-pedantic")
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+      set(ADDITIONAL_COMPILER_FLAGS "-Wall;-Werror;-Wextra")
+    endif()
+  endif()
+
+  if (CMAKE_BUILD_TYPE STREQUAL "Release" OR "RelWithDebInfo" OR "MinSizeRel")
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT ipo_supported)
+
+    if (ipo_supported)
+      set_target_properties(${TARGET_NAME} PROPERTIES
+                            INTERPROCEDURAL_OPTIMIZATION TRUE)
+    endif()
+  endif()
+
+  target_compile_options(${TARGET_NAME} PRIVATE ${MAIN_COMPILER_FLAGS}
+                                                ${ADDITIONAL_COMPILER_FLAGS})
+endfunction()
