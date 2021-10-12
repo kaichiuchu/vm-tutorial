6aa1f2f3978c10c91ce7827469eae9c6f0ffb800
kaichiuchu <kaichiuchu@protonmail.com>
2021-10-12 18:46:03 -0400

    frontend: fix frame limiter and graphics
    For some very strange reason, an extra `glBindTexture()` function call
    was necessary to get OpenGL to play nice with Qt. Prior to this change,
    resizing the window was the only way to actually display the updates to
    the graphics. I don't have a solid explanation for this, yet. All I know
    is that this works with no obvious ill effects. I'm not even sure this
    is an optimal solution...
    
    The frame limiter had a serious flaw on Windows. The granularity of
    the `Sleep()` function on Windows (which `QThread::msleep()` uses) is
    ~16ms, which is sad news because 60FPS is ~16ms. Generally, CHIP-8
    frames take (or rather should take on average) 0ms and the default FPS
    target is, you guessed it, ~16ms. Executing the original code on Windows
    causes the frame limiter to slow the virtual machine to half of the
    requested FPS.
    
    The new code makes use of a deadline concept using the native C++
    threading facilities.
    
    On a slightly unrelated note, we should use as much of the C++ standard
    library as possible, as that should be taking precedence over a number
    of Qt's duplicated facilities...

--- src/frontend/vm_thread.cpp
+++ src/frontend/vm_thread.cpp
@@ -12,6 +12,8 @@
 
 #include "vm_thread.h"
 
+#include <thread>
+
 VMThread::VMThread(QObject* parent_object) noexcept : QThread(parent_object) {
   vm_instance_.update_screen_func_ =
       [this](const chip8::ImplementationInterface::Framebuffer& framebuffer) {
@@ -24,43 +26,82 @@ VMThread::VMThread(QObject* parent_object) noexcept : QThread(parent_object) {
 }
 
 void VMThread::run() noexcept {
-  QElapsedTimer fps_counter;
-  fps_counter.start();
+  // We pull in this inline namespace to make parts of the frame limiter code
+  // more clear.
+  using namespace std::chrono_literals;
 
+  // The deadline time point is used to determine at what future point in time
+  // the thread should stop sleeping. We start at the current time point because
+  // we're not *starting* the run loop in the past or in the future.
+  auto deadline_time_point = std::chrono::steady_clock::now();
+
+  // The FPS (frames per second) time point is used to determine when 1 second
+  // has passed to notify listeners of performance information, should anyone
+  // care. We start at the current time point because we're not *starting* the
+  // run loop in the past or in the future.
+  auto fps_time_point = std::chrono::steady_clock::now();
+
+  // No frames have taken place yet, clear the frame counter so we don't report
+  // specious results to the user who may care about performance information.
   num_frames_ = 0;
 
+  // This thread will continue running until an interrupt is requested by a call
+  // to \ref QThread::requestInterruption(). The only time the run loop will be
+  // interrupted is if one of the following conditions are met:
+  //
+  // 1) an error occurred within the guest program
+  // 2) the guest program is waiting for a key press
+  // 3) the user has paused execution of the virtual machine
   while (!isInterruptionRequested()) {
-    if (fps_counter.hasExpired(1000)) {
+    // The length of a frame in milliseconds can be retrieved by a call to the
+    // \ref chip8::VMInstance::GetMaxFrameTime() method.
+    //
+    // We want the thread to stop sleeping after one frame. Update the deadline
+    // accordingly.
+    deadline_time_point += std::chrono::milliseconds(
+        static_cast<int>(vm_instance_.GetMaxFrameTime()));
+
+    // We need to retrieve the current point in time...
+    auto current_time_point = std::chrono::steady_clock::now();
+
+    // ...to determine the difference between the current point in time and the
+    // last time we checked if we emitted performance information.
+    const auto fps_update_delta =
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+            current_time_point - fps_time_point);
+
+    // Has one second passed since we emitted performance information?
+    if (fps_update_delta >= 1s) {
       emit PerformanceInfo({num_frames_,
-                            (1000.0 / static_cast<double>(num_frames_)),
+                            1000.0 / static_cast<double>(num_frames_),
                             vm_instance_.GetTargetFrameRate()});
 
       num_frames_ = 0;
-      fps_counter.restart();
+
+      // We just emitted the performance information, we'll need to update the
+      // time point to keep track of the passage of time.
+      fps_time_point = std::chrono::steady_clock::now();
     }
 
-    QElapsedTimer timer;
-    timer.start();
-
+    // Now run the virtual machine for one frame.
     const auto step_result = vm_instance_.RunForOneFrame();
     num_frames_++;
 
     if (step_result != chip8::StepResult::kSuccess) {
+      // A condition has been met in which we have to stop execution of the
+      // virtual machine.
       quit();
 
+      // It is not a failure to stop execution until a key has been pressed. We
+      // completely stop execution in this case because it would be pointless
+      // to run the thread doing absolutely nothing.
       if (step_result != chip8::StepResult::kHaltUntilKeyPress) {
         emit ExecutionFailure(step_result);
       }
       break;
     }
 
-    const auto elapsed_frame_time = timer.elapsed();
-    const auto max_frame_time =
-        static_cast<qint64>(vm_instance_.GetMaxFrameTime());
-
-    if (elapsed_frame_time < max_frame_time) {
-      QThread::msleep(
-          static_cast<unsigned long>(max_frame_time - elapsed_frame_time));
-    }
+    // We're done here; sleep until the deadline.
+    std::this_thread::sleep_until(deadline_time_point);
   }
 }
